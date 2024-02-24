226eefca98e1bf0c6460d273671f133c22946693
CathLee <447932704@qq.com>
2024-02-24 21:07:18 +0800

    feat:handleClick of tab click

--- src/lessons_01/comments/index.tsx
+++ src/lessons_01/comments/index.tsx
@@ -1,17 +1,31 @@
-import { FC, useRef, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import avatar from "../images/avatar.jpg";
 import { v4 as uuidv4 } from "uuid";
 import "./index.css";
+import classNames from "classnames";
+interface User {
+  // 假设用户对象有 id 和 name，根据实际情况调整
+  uid: string;
+  uname: string;
+  avatar: string;
+}
 
-const user = {
+interface Comment {
+  rpid: string; // 假设 rpid 是一个字符串类型的 UUID
+  user: User; // 使用上面定义的 User 接口
+  content: string; // 内容为字符串类型
+  ctime: string; // 创建时间为字符串类型，格式为 "MM-DD HH: MM"
+  like: number; // 喜欢的数量为数字类型
+}
+const user: User = {
   uid: "13258165",
   avatar: avatar,
   uname: "周杰伦",
 };
 
-const list = [
+const list: Comment[] = [
   {
-    rpid: '3',
+    rpid: "3",
     user: {
       uid: "13258165",
       avatar: "http://toutiao.itheima.net/resources/images/98.jpg",
@@ -22,7 +36,7 @@ const list = [
     like: 126,
   },
   {
-    rpid: '1',
+    rpid: "1",
     user: {
       uid: "30009257",
       avatar: avatar,
@@ -33,7 +47,7 @@ const list = [
     like: 66,
   },
   {
-    rpid: '2',
+    rpid: "2",
     user: {
       uid: "36080105",
       avatar: "http://toutiao.itheima.net/resources/images/98.jpg",
@@ -50,12 +64,13 @@ const tabs = [
   { type: "time", text: "最新" },
 ];
 const Comments: FC = () => {
-  const [commentList, setCommentList] = useState(list);
+  const [commentList, setCommentList] = useState<Comment[]>(list);
   const [content, setContent] = useState("");
-  const inputRef = useRef(null);
-  
+  const [type, setType] = useState<string>("");
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
   const handleSubmit = () => {
-    const param = {
+    const param: Comment = {
       rpid: uuidv4(),
       user,
       content,
@@ -65,11 +80,70 @@ const Comments: FC = () => {
     // 使用不可变更新数据
     const newList = [...commentList, param];
     setCommentList(newList);
+    setContent('')
+    // 2.聚焦 dom(useRef) - focus
+    inputRef.current?.focus()
   };
+
+  const handleClickTab = (type: string) => {
+    setType(type);
+
+    // 复制原数组以避免直接修改状态
+    let newList = [...commentList];
+    if (type === "hot") {
+      // 对副本进行排序
+      newList.sort((a, b) => b.like - a.like);
+    } else if (type === "time") {
+      // 对副本进行排序
+      newList.sort((a, b) => {
+        const dateA = new Date(
+          a.ctime.replace(
+            /(\d{2})-(\d{2}) (\d{2}):(\d{2})/,
+            "2024-$1-$2T$3:$4:00"
+          )
+        );
+        const dateB = new Date(
+          b.ctime.replace(
+            /(\d{2})-(\d{2}) (\d{2}):(\d{2})/,
+            "2024-$1-$2T$3:$4:00"
+          )
+        );
+        return dateB.getTime() - dateA.getTime();
+      });
+    }
+
+    // 使用排序后的新列表更新状态
+    setCommentList(newList);
+  };
+  useEffect(() => {
+    console.log("Comment list updated", commentList);
+  }, [commentList]);
   return (
     <>
       <div className="app">
-        <div className="reply-navigation"></div>
+        <div className="reply-navigation">
+          <ul className="nav-bar">
+            <li className="nav-title">
+              <span className="nav-title-text">评论</span>
+              <span className="total-reply">{10}</span>
+            </li>
+            <li className="nav-sort">
+              {tabs.map((item) => {
+                return (
+                  <span
+                    key={item.type}
+                    className={classNames("nav-item", {
+                      active: type === item.type,
+                    })}
+                    onClick={() => handleClickTab(item.type)}
+                  >
+                    {item.type}
+                  </span>
+                );
+              })}
+            </li>
+          </ul>
+        </div>
         <div className="reply-wrap">
           <div className="box-normal">
             <div className="reply-box-avatar">
