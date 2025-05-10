657626a251aca6d413a03d425739b70b25c6c1b9
CathLee <447932704@qq.com>
2025-05-10 15:37:19 +0800

    wip:简易的preload plugin

--- src/plugin/preload.js
+++ src/plugin/preload.js
@@ -1,75 +1,37 @@
+/*
+ * @Date: 2025-05-09 22:48:46
+ * @Description: 
+ */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// Create a custom plugin to add preload links
 class CustomPreloadPlugin {
-  constructor(options = {}) {
-    this.options = options;
-  }
-
   apply(compiler) {
     compiler.hooks.compilation.tap('CustomPreloadPlugin', compilation => {
-      // Check if HtmlWebpackPlugin is used
-      if (compilation.hooks.htmlWebpackPluginAfterHtmlProcessing) {
-        // For HtmlWebpackPlugin v3
-        compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
-          'CustomPreloadPlugin',
-          this.processHTML.bind(this)
-        );
-      } else {
-        // For HtmlWebpackPlugin v4 or later
-        const hooks = HtmlWebpackPlugin.getHooks(compilation);
-        hooks.beforeEmit.tapAsync(
-          'CustomPreloadPlugin',
-          this.processHTML.bind(this)
-        );
-      }
+      // This hook runs after HTML is processed
+      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+        'CustomPreloadPlugin',
+        (data, cb) => {
+          // Image paths to preload - update these with your actual paths from console.log
+          const imagePaths = [
+            '/static/media/aa.61df1d7189f9aa63ad7.jpg',
+            '/static/media/download.7e7ee98b2ea8febed407.jpg',
+            '/static/media/asuka.90f97d8af0d242106d74.jpg'
+          ];
+          
+          // Add preload links before the closing head tag
+          const preloadLinks = imagePaths.map(path => 
+            `<link rel="preload" as="image" href="${path}">`
+          ).join('\n');
+          
+          // Insert before closing head tag
+          data.html = data.html.replace('</head>', `${preloadLinks}\n</head>`);
+          console.log('Preload links added:', preloadLinks);
+          console.log('HTML after adding preload links:', data.html);
+          cb(null, data);
+        }
+      );
     });
-  }
-
-  processHTML(data, callback) {
-    const outputAssets = data.assets || [];
-    const publicPath = data.publicPath || '';
-    
-    // Find all JPG/JPEG assets
-    const jpgAssets = [];
-    
-    // For different webpack versions
-    if (Array.isArray(outputAssets)) {
-      // HtmlWebpackPlugin v3
-      outputAssets.forEach(asset => {
-        if (/\.(jpg|jpeg)$/i.test(asset)) {
-          jpgAssets.push(asset);
-        }
-      });
-    } else {
-      // For modern webpack with different asset structure
-      console.log('Searching for JPG assets in compilation');
-      
-      // Print out all assets for debugging
-      const assetKeys = Object.keys(outputAssets);
-      console.log('All assets:', assetKeys);
-      
-      // Filter JPG assets
-      assetKeys.forEach(key => {
-        if (/\.(jpg|jpeg)$/i.test(key)) {
-          jpgAssets.push(publicPath + key);
-        }
-      });
-    }
-    
-    console.log('Found JPG assets for preloading:', jpgAssets);
-    
-    // Generate preload links
-    const preloadLinks = jpgAssets.map(asset => 
-      `<link rel="preload" as="image" href="${asset}">`
-    ).join('\n');
-    
-    // Insert before closing head tag
-    if (preloadLinks.length > 0) {
-      console.log('Adding preload links to HTML');
-      data.html = data.html.replace('</head>', `${preloadLinks}\n</head>`);
-    }
-    
-    callback(null, data);
   }
 }
 
